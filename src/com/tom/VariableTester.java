0c26132480fb2944426fb73830000a022ace51e4
IrisAndMimi <my1ris227@outlook.com>
2017-10-17 16:33:47 +0800

    variable, primitive

---
+++ src/com/tom/VariableTester.java
@@ -0,0 +1,28 @@
+package com.tom;
+
+import java.util.Random;
+
+public class VariableTester {
+
+	public static void main(String[] args) {
+		int age = 0;
+		float weight = 0.0f;	//轉型
+		char c = 65;
+		char c2 = 'Z';
+//		
+		int i = c2;
+		System.out.println(i);
+//		System.out.println((int)c2);
+		boolean pass = true;
+		System.out.println(pass);
+		
+		String s = new String("Hello");
+		Random r = new Random();
+		
+		System.out.println(r.nextInt());
+		int n1  = r.nextInt(6)+1;
+		
+		System.out.println(!(3 == 2));
+	}
+		
+}
